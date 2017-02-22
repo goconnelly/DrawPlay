b338b067cf891c7c45037668cf7b30f63dc9541d
Megan Kallis <mkallis@mines.edu>
2017-02-21 20:36:31 -0700

    Cat says meow three times now instead of one

--- src/Cat.java
+++ src/Cat.java
@@ -42,8 +42,8 @@ public class Cat {
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
 		g2.setColor(Color.black);
-		// Meow text appears below cat head, +10 places below 
+		// Meow meow meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("Meow meow meow", catX, catY+HEAD_DIMENSION+10);	
 	}
 }
